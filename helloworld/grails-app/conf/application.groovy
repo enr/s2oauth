f02c534fd727bcef1801d80d203be6a13ab1f49f
enr <enrico.comiti@gmail.com>
2015-09-30 23:39:57 +0200

    move SSC configuration to yml

--- helloworld/grails-app/conf/application.groovy
+++ helloworld/grails-app/conf/application.groovy
@@ -1,20 +1,20 @@
 
 
 // Added by the Spring Security Core plugin:
-grails.plugin.springsecurity.userLookup.userDomainClassName = 'helloworld.User'
-grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'helloworld.UserRole'
-grails.plugin.springsecurity.authority.className = 'helloworld.Role'
-grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	'/':                ['permitAll'],
-	'/error':           ['permitAll'],
-	'/index':           ['permitAll'],
-	'/index.gsp':       ['permitAll'],
-	'/shutdown':        ['permitAll'],
-	'/assets/**':       ['permitAll'],
-	'/**/js/**':        ['permitAll'],
-	'/**/css/**':       ['permitAll'],
-	'/**/images/**':    ['permitAll'],
-	'/**/favicon.ico':  ['permitAll'],
-	// s2oauth
-  '/oauth/**':        ['permitAll']
-]
+// grails.plugin.springsecurity.userLookup.userDomainClassName = 'helloworld.User'
+// grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'helloworld.UserRole'
+// grails.plugin.springsecurity.authority.className = 'helloworld.Role'
+// grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+// 	'/':                ['permitAll'],
+// 	'/error':           ['permitAll'],
+// 	'/index':           ['permitAll'],
+// 	'/index.gsp':       ['permitAll'],
+// 	'/shutdown':        ['permitAll'],
+// 	'/assets/**':       ['permitAll'],
+// 	'/**/js/**':        ['permitAll'],
+// 	'/**/css/**':       ['permitAll'],
+// 	'/**/images/**':    ['permitAll'],
+// 	'/**/favicon.ico':  ['permitAll'],
+// 	// s2oauth
+//   '/oauth/**':        ['permitAll']
+// ]
